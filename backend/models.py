cdf6fa205aec051bedb102931d788e5f58246b66
john potter <juanpotter7@gmail.com>
2023-12-18 23:56:29 -0500

    init and model setup

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Caffeine(models.Model):
+    mg = models.IntegerField(default=0)
+    time = models.TimeField()
+
+class Exercise(models.Model):
+    time = models.IntegerField(default=0)
+    intensity = models.IntegerField()
+
+class Sleep(models.Model):
+    hours = models.FloatField()
+    quality = models.IntegerField()
---
+++ backend/serializers.py
@@ -0,0 +1,1 @@
+from .models import Caffeine, Exercise, Sleep
