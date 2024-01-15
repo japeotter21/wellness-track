64a9040aff3aa919c0192c21e3d1357d6483b5b9
john potter <juanpotter7@gmail.com>
2024-01-14 22:23:18 -0500

    fix cors from frontend

--- wellness/settings.py
+++ wellness/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,10 +51,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    
 ]
-CORS_ORIGIN_ALLOW_ALL= True
-
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://192.168.1.103:3000', 'http://localhost:3000']
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 ROOT_URLCONF = 'wellness.urls'
 
 TEMPLATES = [
